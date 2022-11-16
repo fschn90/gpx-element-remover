c3e81a9c55af1b3cc766ef765946b554d4c9fc68
098714 <fschn@fschn.org>
2022-11-16 13:22:50 +0100

    first commit, feature drop

---
+++ README.md
@@ -0,0 +1,5 @@
+to remove time element in gpx file
+
+specify file in line 7
+
+run node gpx-ending-time in terminal
---
+++ gpx-ending-time.js
@@ -0,0 +1,31 @@
+const xml2js = require('xml2js');
+const fs = require('fs');
+const { time } = require('console');
+const parser = new xml2js.Parser({ attrkey: "$" });
+
+// importing gpx and saving as object
+let xml_string = fs.readFileSync("africaAggregate.gpx", "utf8");
+var gpxFile = {};
+
+parser.parseString(xml_string, function (error, result) {
+    for (var i in result) {
+        gpxFile[i] = result[i]
+    }
+});
+
+//remove time key in object 
+for (var x in gpxFile.gpx.trk) {
+    for (var k in gpxFile.gpx.trk[x].trkseg) {
+        for (var j in gpxFile.gpx.trk[x].trkseg[k].trkpt) {
+            delete gpxFile.gpx.trk[x].trkseg[k].trkpt[j].time
+        }
+    }
+}
+
+//save time removed object as gpx file
+var builder = new xml2js.Builder();
+var xml = builder.buildObject(gpxFile);
+fs.writeFile('Output.gpx', xml, (err) => {
+    if (err)
+        console.log(err);
+})
